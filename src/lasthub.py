1f8dbbbe56bfddd2312bae8fcc04010df7f7c02a
Chris Hannam <bassdread@gmail.com>
2011-06-06 13:38:50 +0100

    added new date based fetching

--- src/lasthub.py
+++ src/lasthub.py
@@ -4,8 +4,9 @@ use_library('django', '1.2')
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import urlfetch
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils import simplejson
+from django.utils.datastructures import SortedDict
 import os, time
 from google.appengine.ext.webapp import template
 
@@ -39,33 +40,14 @@ class Index(webapp.RequestHandler):
 
             template_values = {'commits' : []}
             
-            date, time, offset = github[-1]['created_at'].rsplit(' ')
-            start_time = datetime.strptime(date + ' ' + time, '%Y/%m/%d %H:%M:%S')
-            
-            date, time, offset = github[1]['created_at'].rsplit(' ')
-            end_time = datetime.strptime(date + ' ' + time, '%Y/%m/%d %H:%M:%S')
-            
+            for commit in github:
+                track = self._fetch_lastfm(lastfm_username, commit['commit_time'])
+                if track:
+                    commit['track'] = track
+                    commit['album'] = track['album']['#text']
+                    commit['artist'] = track['artist']['#text']
 
-            lastfm = self._fetch_lastfm(lastfm_username, start_time, end_time)
-            
-            for git in github:
-                date, time, offset = git['created_at'].rsplit(' ')
-                a = datetime.strptime(date + ' ' + time, '%Y/%m/%d %H:%M:%S')
-                git['created_at'] = a
-                for track in lastfm['recenttracks']['track'][1:]:
-                    b = datetime.fromtimestamp(float(track['date']['uts']))
-                    c = a - b
-                    if c.seconds < 240 and c.days == 0:
-                        git['track'] = track
-                        git['album'] = track['album']['#text']
-                        git['artist'] = track['artist']['#text']
-                        try:
-                            #git['comment'] = git['payload']['shas'][0][2]
-                            git['comment'] = self._set_title_body(git)
-                        except:
-                            pass
-                        template_values['commits'].append(git)
-                        break
+                    template_values['commits'].append(commit)
             
         else:
             template_values = {'missing_username' : True}
@@ -113,31 +95,51 @@ class Index(webapp.RequestHandler):
         
         user_json = urlfetch.fetch(url, validate_certificate=False)
         
-        if user_json.status_code == 200:
-            return simplejson.loads(user_json.content)
+        if not user_json.status_code == 200:
+            return
+        
+        user_events = simplejson.loads(user_json.content)            
+        
+        commits = []
+        
+        for event in user_events:
+            if event['type'] == 'PushEvent':
+                url = "https://github.com/api/v2/json/commits/show/%s/%s/%s" % (event['repository']['owner'], event['repository']['name'], event['payload']['shas'][0][0])
+                
+                commit_json = urlfetch.fetch(url, validate_certificate=False)
             
-    def _fetch_lastfm(self, username, start_time, end_time):
+                if commit_json.status_code == 200:
+                    commit = simplejson.loads(commit_json.content)
+                    
+                    # build dict for use later
+                    date_time, offset = commit['commit']['committed_date'].rsplit('-', 1)
+                    start_time = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
+                    commit['commit_time'] = start_time + timedelta(hours=int(offset.split(':')[0]))
+                    commit['repo'] = event['repository']['name']
+                    commits.append(commit)
+            
+        return commits
+
+    
+    def _fetch_lastfm(self, username, commit_time):
         """Fetch all the detials for a user in github."""
 
-        start = str(int(time.mktime(start_time.timetuple())))
+        offset = timedelta(minutes=10)
+        start = str(int(time.mktime(commit_time.timetuple())))
+        end_time = commit_time + offset
         end = str(int(time.mktime(end_time.timetuple())))
         
-        url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=09f1c061fc65a7bc08fb3ad95222d16e&format=json&limit=200&from=%s&to=%s" % (username, start, end)
+        url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=ADD_KEY!&format=json&limit=200&from=%s&to=%s" % (username, start, end)
         
         user_json = urlfetch.fetch(url, validate_certificate=False)
         
         if user_json.status_code == 200:
             json = simplejson.loads(user_json.content)
             try:
-                if json['recenttracks']['@attr']['totalPages'] > str(1):
-                    for i in range(2,4):
-                        url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=ADD_KEY!&format=json&limit=200&from=%s&to=%s&page=%s" % (username, start, end, i)
-                        user_json = urlfetch.fetch(url, validate_certificate=False)
-                        tmp_json = simplejson.loads(user_json.content)
-                        json['recenttracks']['track'] = json['recenttracks']['track'] + tmp_json['recenttracks']['track']
+                return json['recenttracks']['track'][0]
             except:
-                pass
-        return json
+                return
+    
 application = webapp.WSGIApplication(
                                      [('/', Index)],
                                      debug=True)
